1d5b5e19dd9c4e3d17acf7dd1fd8402eec058044
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-20 14:57:34 -0600

    created an api file and use GEO API

---
+++ src/api.jsx
@@ -0,0 +1,8 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d5147c55e8msh40c9374af90e985p144103jsnf2062ff1a1cf",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
