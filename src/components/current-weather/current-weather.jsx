7fd539e7762b8f7c19293ff8cccd1f11823f9982
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-22 16:30:54 -0600

    display the correct value for each details

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -16,7 +16,7 @@ const CurrentWeather = ({ data }) => {
         />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label">Details</span>
@@ -24,22 +24,24 @@ const CurrentWeather = ({ data }) => {
 
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">22°C</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
 
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
 
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
 
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hpa</span>
+            <span className="parameter-value">{data.main.pressure} hpa</span>
           </div>
         </div>
       </div>
