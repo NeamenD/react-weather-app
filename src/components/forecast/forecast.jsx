0039c5319e2c00a25384ba45d80a9d9d1cc55e5e
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-23 20:25:22 -0600

    added a slice operation to limit the forecast to 7 days.

--- src/components/forecast/forecast.jsx
+++ src/components/forecast/forecast.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "./forecast.css";
 
-export default function Forecast() {
-  return <div>forecast</div>;
-}
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+  console.log(forecastDays);
+
+  return (
+    <div className="forecast-container">
+      <h2 className="forecast-title">Weekly Forecast</h2>
+      <Accordion allowZeroExpanded>
+        {data &&
+          data.list &&
+          data.list.splice(0, 7).map((item, idx) => (
+            <AccordionItem key={idx}>
+              <AccordionItemHeading>
+                <AccordionItemButton>
+                  <h1>hello </h1>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+            </AccordionItem>
+          ))}
+      </Accordion>
+    </div>
+  );
+};
+
+export default Forecast;
